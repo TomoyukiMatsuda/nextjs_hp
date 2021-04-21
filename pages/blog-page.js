9087e22aff515e47749fc6518eff3e96d9028473
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-04-22 07:59:58 +0900

    不要なオブジェクト変換処理を削除/コメント記載

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -2,7 +2,7 @@ import Layout from '../components/Layout';
 import Post from '../components/Post';
 import { getAllPostsData } from '../lib/posts';
 
-
+// getStaticProps() の返り値を props(posts) で受け取り
 const Blog = ({ posts }) => {
   return <Layout title="Blog">
     <ul className="m-10">
@@ -16,8 +16,10 @@ const Blog = ({ posts }) => {
 
 export default Blog;
 
+// 必ずビルドの際事前にサーバーサイド側で実行される
 export async function getStaticProps() {
   const posts = await getAllPostsData();
+  // return した値を Blog の props(posts)で受け取っている
   return {
     props: { posts } ,
   };
